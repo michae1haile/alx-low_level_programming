32ae900608b1e2f3b6bbd7cbb4fb4580d31fbdf2
michael1haile <haile12black12@gmail.com>
2023-02-21 14:18:23 -0800

    function update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@
 void print_fib(int len)
 {
 	unsigned int target, i;
-	unsigned int former_l,  former_r, ;
+	unsigned int former_l, former_r;
 	unsigned int latter_l, latter_r;
 	unsigned int fib_l, fib_r;
 
