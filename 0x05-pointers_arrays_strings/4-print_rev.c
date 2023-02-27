1e7d44abb988a1b1ebe72c3f7b4da2c931fc9808
michael1haile <haile12black12@gmail.com>
2023-02-27 08:05:03 -0800

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,8 @@
 /**
  * print_rev - a function that prints a string, in reverse
  * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
  */
 void print_rev(char *s)
 {
